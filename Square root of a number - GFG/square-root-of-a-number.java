918c5b7d7faaea1c26f9a4d54fd19e0088bedeb6
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-01-18 18:59:21 +0530

    Added solution - LeetHub

---
+++ Square root of a number - GFG/square-root-of-a-number.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+
+class SquartRoot
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			long a = sc.nextInt();
+			Solution obj = new Solution();
+			System.out.println(obj.floorSqrt(a));
+		t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/*You are required to complete
+this function*/
+
+// Function to find square root
+// x: element to find square root
+class Solution
+{
+     long floorSqrt(long x)
+	 {
+		// Your code here
+		int i=1;
+		for(; i*i<=x; i++){
+		    if(i*i==x){
+		        return i;
+		    }
+		}
+		return i-1;
+	 }
+}
