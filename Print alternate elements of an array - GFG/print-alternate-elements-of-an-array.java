c6f67c40619a84ac175b936d02258200b7672041
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-01-21 23:12:38 +0530

    Added solution - LeetHub

--- Print alternate elements of an array - GFG/print-alternate-elements-of-an-array.java
+++ Print alternate elements of an array - GFG/print-alternate-elements-of-an-array.java
@@ -36,7 +36,7 @@ class GfG
     public static void print(int arr[], int n)
     {
         // your code here
-        for(int i=0;i<n; i+=2){
+        for(int i=0;i<n; i=i+2){
                 System.out.print(arr[i]+" ");
         }
         
