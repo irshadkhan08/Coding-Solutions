7592975d00de209aea9da9943206b520629cac30
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-02-02 02:45:02 +0530

    Added solution - LeetHub

--- Common elements - GFG/common-elements.java
+++ Common elements - GFG/common-elements.java
@@ -61,32 +61,28 @@ class Solution
     ArrayList<Integer> commonElements(int A[], int B[], int C[], int n1, int n2, int n3) 
     {
         // code here 
-        int i =0;
-        int j =0;
-        int k =0;
-        Set<Integer>set = new HashSet<Integer>();
-         ArrayList<Integer>ans = new ArrayList<Integer>();
-        
+         int i =0,j =0,k =0;
+         
+         ArrayList<Integer>ans = new ArrayList<>();
+         Set<Integer>set = new HashSet<Integer>();
          while(i<n1 && j<n2 && k<n3){
-            
-                    if((A[i]==B[j])&& (B[j]==C[k])){
-                        if(set.add(A[i]))
-                        ans.add(A[i]);
-                        i++;
-                        j++;
-                        k++;
-                     
-                       
-                    }
-                    else if(A[i]<B[j]){
-                        i++;
-                    }
-                    else if(B[j]<C[k]){
-                        j++;
-                    }else{
-                        k++;
-                    }  
-         }  
-        return ans;
+            if(A[i]==B[j] && B[j]==C[k]){
+                if(set.add(A[i]))
+                ans.add(C[k]);
+                i++;
+                j++;
+                k++;
+            }
+            else if(A[i]<B[j]){
+                i++;
+            }
+            else if(B[j]<C[k]){
+                j++;
+            }
+            else if(C[k]<A[i]){
+                k++;
+            }
+         }
+         return ans;
     }
 }
