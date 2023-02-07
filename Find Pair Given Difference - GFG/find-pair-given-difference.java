7fff98b01450f87f132c6ef5a07a48861f1110fa
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-02-07 10:25:25 +0530

    Added solution - LeetHub

--- Find Pair Given Difference - GFG/find-pair-given-difference.java
+++ Find Pair Given Difference - GFG/find-pair-given-difference.java
@@ -49,7 +49,10 @@ class Solution
         while(i<size && j<size){
             
             if(i!=j && (arr[j]-arr[i]==n || arr[i]-arr[j]==n)){
+                i++;
+                j++;
                 return true;
+                
                 
             }else if(arr[j]-arr[i]<n){
                 j++;
