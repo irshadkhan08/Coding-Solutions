427b432ffb75624d4755c35753733966afd14d9e
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-02-13 09:45:21 +0530

    Added solution - LeetHub

---
+++ Intersection of two arrays - GFG/intersection-of-two-arrays.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+    public static void main(String[] args) {
+
+        // Taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Taking count of total testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n, m;
+
+            // taking count of elements in array a
+            n = sc.nextInt();
+
+            // taking count of elements in array b
+            m = sc.nextInt();
+
+            // Creating 2 arrays of n and m
+            int a[] = new int[n];
+            int b[] = new int[m];
+
+            // inserting elements to array a
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            // inserting elements to array b
+            for (int i = 0; i < m; i++) {
+                b[i] = sc.nextInt();
+            }
+            Solution ob = new Solution();
+            // calling NumberofElementsInIntersection method
+            // and printing the result
+            System.out.println(ob.NumberofElementsInIntersection(a, b, n, m));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to return the count of the number of elements in
+    // the intersection of two arrays.
+    public static int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
+        // Your code here
+        
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        
+        Set<Integer>set1 = new HashSet<Integer>();
+        for(int e1 : a){
+            set1.add(e1);
+        }
+        
+        Set<Integer>set2 = new HashSet<Integer>();
+        for(int e2 : b){
+            set2.add(e2);
+        }
+        
+        Set<Integer>common = new HashSet<Integer>();
+        
+        for(int value : set1){
+            if(set2.contains(value)){
+              common.add(value);
+            }
+        }
+        
+        // HashSet<Integer>res = 
+        
+        
+         return common.size();
+    }
+}
