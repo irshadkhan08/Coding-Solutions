ab5c9744ecb56e15cd943a5ae4c3594250e85a47
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-03-12 22:53:19 +0530

    Added solution - LeetHub

--- Largest Element in Array - GFG/largest-element-in-array.java
+++ Largest Element in Array - GFG/largest-element-in-array.java
@@ -37,8 +37,8 @@ class GFG {
 class Compute {
     public int largest(int arr[], int n)
     {
-        if(n==0)return 0;
-        return Math.max(arr[n-1],largest(arr,n-1));
+        if(n==0)return arr[0];
+        return Math.max(largest(arr,n-1),arr[n-1]);
     }
 }
 
