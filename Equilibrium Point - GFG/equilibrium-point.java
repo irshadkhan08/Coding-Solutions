ff4b77a46346e49798741d7fcaf201781cba1c4c
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-04-03 14:04:33 +0530

    Added solution - LeetHub

---
+++ Equilibrium Point - GFG/equilibrium-point.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+            
+            //taking input n
+            int n = Integer.parseInt(br.readLine().trim());
+            long arr[] = new long[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            
+            //adding elements to the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = Long.parseLong(inputLine[i]);
+            }
+
+            Solution ob = new Solution();
+            
+            //calling equilibriumPoint() function
+            System.out.println(ob.equilibriumPoint(arr, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    
+    // a: input array
+    // n: size of array
+    // Function to find equilibrium point in the array.
+    public static int equilibriumPoint(long arr[], int n) {
+
+        // Your code here
+        int ans = -1;
+        // if(n==0){
+        //     return -1;
+        // }
+        if(n==1){
+            return 1;
+        }
+        long totalSum = 0;
+        
+        for(int i = 0; i<n; i++){
+            totalSum+=arr[i];
+        }
+        long sum = 0;
+        
+        for(int i =1; i<n; i++){
+            sum = sum+arr[i-1];
+            if((totalSum-sum-arr[i])==sum){
+             ans = i;
+             break;
+            }
+            
+        }
+        if(ans==-1){
+            return ans;
+        }
+        else{
+            return ans+1;
+        }
+    }
+}
