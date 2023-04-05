ac311a6849650f3701df70eca28b52f6ad74eff7
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-04-06 03:27:40 +0530

    Added solution - LeetHub

---
+++ Equal Left and Right Subarray Sum - GFG/equal-left-and-right-subarray-sum.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int test = Integer.parseInt(br.readLine());
+		while(test-- > 0) {
+			int N = Integer.parseInt(br.readLine());
+			int [] A = new int[N];
+			String [] str = br.readLine().trim().split(" ");
+			for(int i = 0; i < N; i++)
+				A[i] = Integer.parseInt(str[i]);
+			Solution ob = new Solution();
+			System.out.println(ob.equalSum(A, N));
+		}
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+	int equalSum(int [] A, int N) {
+		//Write your code here
+		int totalSum = 0;
+		for(int i = 0; i<N; i++){
+		    totalSum += A[i];
+		}
+		
+		int sum = 0;
+		int ans = -1;
+		for(int i = 0; i<N; i++){
+		     sum+=A[i];
+		    if(totalSum==sum){
+		        ans = i;
+		        break;
+		    }
+		    totalSum = totalSum-A[i];
+		}
+		
+	   if(ans==-1){
+	       return ans;
+	   }
+	   else{
+	       return ans+1;
+	   }
+	}
+}
