188b78f647d5d91120f9a460e3e29444e1449737
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-04-14 20:26:59 +0530

    Added solution - LeetHub

---
+++ Transpose of Matrix - GFG/transpose-of-matrix.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG
+{
+    public static void main (String[] args)throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t= Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(read.readLine());
+            int a[][] = new int[n][n];
+            String st[] = read.readLine().trim().split("\\s+");
+            int k = 0;
+            for(int i = 0; i < n; i++)
+                for(int j = 0; j < n; j++)
+                    a[i][j] = Integer.parseInt(st[k++]);
+
+            Solution ob = new Solution();
+            ob.transpose(a, n);
+            for(int i = 0; i < n; i++)
+                for(int j = 0; j < n;j++)
+                    System.out.print(a[i][j] + " ");
+            
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find transpose of a matrix.
+    static void transpose(int matrix[][], int n)
+    {
+        // code here
+        
+        for(int i = 0; i<matrix.length; i++){
+           
+            for(int j = 0; j<=i; j++){
+                swap(i,j,matrix);
+               // matrix[i][j] = matrix[j][i];
+            }
+        }
+    }
+    public static void swap(int i,int j,int matrix[][]){
+        int temp = matrix[j][i];
+        matrix[j][i] = matrix[i][j];
+        matrix[i][j] = temp;
+    }
+}
