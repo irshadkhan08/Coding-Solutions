742223fe9411e3f995a4605eb3dcd65f4e23c2b8
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-04-14 11:44:05 +0530

    Added solution - LeetHub

--- Replace all 0's with 5 - GFG/replace-all-0s-with-5.java
+++ Replace all 0's with 5 - GFG/replace-all-0s-with-5.java
@@ -20,9 +20,35 @@ class GfG {
     int convertfive(int num) {
         // Your code here
         
-      String n = String.valueOf(num);
-      n = n.replaceAll("0","5");
-      return Integer.parseInt(n);
-      
+        // first method
+    //   String n = String.valueOf(num);//"1004"
+    //   n = n.replaceAll("0","5");//"1554"
+    //   return Integer.parseInt(n);//1554
+    
+    //second method
+    
+    int rev = 0;
+    int d = 0;
+    
+    while(0<num){
+        d = num%10;
+        
+        if(d==0){
+            d = 5;
+        }
+        rev = rev*10+d;
+        num = num/10;
+    }
+    
+    int ans = 0;
+    int rem = 0;
+    
+    while(0<rev){
+        rem = rev%10;
+        
+        ans = ans*10+rem;
+        rev = rev/10;
+    }
+    return ans;
     }
 }
