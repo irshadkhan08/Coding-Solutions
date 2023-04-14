5fd20e9a748d81d949fe0ff1621da3c9f4aff146
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-04-15 00:20:18 +0530

    Added solution - LeetHub

---
+++ Multiply Matrices - GFG/multiply-matrices.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+class Matrix{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0){
+			int n = sc.nextInt();
+			int[][] a = new int[n][n];
+			int[][] b = new int[n][n];
+			int[][] c = new int[n][n];
+			for(int i = 0; i < n; i++)
+				for(int j = 0; j < n; j++)
+					a[i][j]=sc.nextInt();
+			for(int i = 0 ;i < n; i++)
+				for(int j = 0; j < n; j++)
+					b[i][j]=sc.nextInt();
+			GfG g = new GfG();
+			g.multiply(a,b,c,n);
+			for (int i = 0; i < n; i++)
+			{for (int j = 0; j < n; j++)
+				System.out.print(c[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+class GfG
+{
+      public static void multiply(int A[][], int B[][], int C[][], int N)
+        {
+           //add code here.
+           int r = A.length;
+           int c = B.length;
+            //C[][] = new int[r][c];
+           
+           for(int i = 0; i<r; i++){
+               
+               for(int j = 0; j<c; j++){
+                   C[i][j] = 0;
+                   for(int k = 0; k<C.length; k++){
+                       C[i][j]+=A[i][k]*B[k][j];
+                   }
+               }
+           }
+           
+           
+           for(int i = 0; i<C.length; i++){
+               for(int j = 0; j<C.length; j++){
+                   A[i][j] = C[i][j];
+               }
+           }
+        }
+}
