4abad6bea8503a7dd849c180a2140674ee27da86
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-04-15 00:10:48 +0530

    Added solution - LeetHub

--- Multiply 2 matrices - GFG/multiply-2-matrices.java
+++ Multiply 2 matrices - GFG/multiply-2-matrices.java
@@ -49,27 +49,18 @@ class Solution
         // code here
       int n1 = matrixA.length;
       int n2 = matrixB.length;
-        // int ans[][] = {
-        //               {0,0,0},
-        //               {0,0,0},
-        //               {0,0,0}
-        //     };
-            
-        int ans[][] = new int[n1][n2];
-      if(n1!=n2){
-          return;
-      }
-      else{
+      
+      int ans[][] = new int[n1][n2];
+      
        for(int i = 0; i<matrixA.length; i++){
            for(int j =0; j<matrixB[0].length; j++){
                ans[i][j] = 0;
-               for(int k = 0; k<matrixA.length; k++){
+               for(int k = 0; k<ans.length; k++){
                  ans[i][j]+= matrixA[i][k]*matrixB[k][j];
                }
            }
        }
        
-      }
       for(int i = 0; i<ans.length; i++){
           for(int j =0; j<ans[0].length; j++){
               matrixA[i][j] = ans[i][j];
