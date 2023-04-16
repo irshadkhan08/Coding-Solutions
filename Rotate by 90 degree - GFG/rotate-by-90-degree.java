729b194e5227f5fef0ff058de5b20265868745ab
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-04-17 00:54:45 +0530

    Added solution - LeetHub

--- Rotate by 90 degree - GFG/rotate-by-90-degree.java
+++ Rotate by 90 degree - GFG/rotate-by-90-degree.java
@@ -1,72 +1,106 @@
 //{ Driver Code Starts
 //Initial Template for Java
 
-import java.util.*; 
 import java.io.*;
-import java.lang.*;
+import java.util.*;
 
-class DriverClass
+class GFG
 {
-	public static void main(String[] args) 
-	{ 
-	    Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+    public static void main(String args[])throws IOException
+    {
+         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t= Integer.parseInt(read.readLine());
         
-        while (t-- > 0)
+        while(t-- > 0)
         {
-            int n = sc.nextInt();
-            int[][] arr = new int[n][n];
-            
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < n; j++)
-                    arr[i][j] = sc.nextInt();
-            
-            GFG g = new GFG();
-            g.rotate(arr);
-            printMatrix (arr);
+            int n = Integer.parseInt(read.readLine());
+            int matrix[][] = new int[n][n];
+            String st[] = read.readLine().trim().split("\\s+");
+            int k = 0;
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                    matrix[i][j] = Integer.parseInt(st[k++]);
+            }
+            Solution ob = new Solution();
+            ob.rotateby90(matrix, n);
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                    System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
         }
-	} 
-	
-	static void printMatrix(int arr[][]) 
-	{ 
-		for (int i = 0; i < arr.length; i++) { 
-			for (int j = 0; j < arr[0].length; j++) 
-				System.out.print(arr[i][j] + " "); 
-			System.out.println(""); 
-		} 
-	} 
+    }
 }
+
 // } Driver Code Ends
 
 
 //User function Template for Java
 
-class GFG
+class Solution
 {
-    static void rotate(int matrix[][]) 
-    {
-        // Code Here
+    //Function to rotate matrix anticlockwise by 90 degrees.
+    static void rotateby90(int matrix[][], int n) 
+    { 
         
         
-   
-       int n = matrix.length;
-       
-        int l = 0;
-       int temp[][] = new int[n][n];
-        for(int i = n-1; i>=0; i--){
-             int k = 0;
-            for(int j = 0; j<n; j++){
-                //matrix[k][j] = matrix[j][i];
-                  temp[l][k++]= matrix[j][i];
+        
+        //method first without using extra space take o(1)space
+        
+        
+        for(int i = 0; i<n;i++){
+            for(int j = 0; j<i; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
-            l++;
-            k = 0;
         }
         
-     for(int i = 0; i<n; i++){
-         for(int j= 0; j<n; j++){
-             matrix[i][j] = temp[i][j];
-         }
-    }
+        int l = 0;
+        int r = n-1;
+        
+        while(l<=r){
+            for(int j= 0; j<n; j++){
+                int temp = matrix[l][j];
+                matrix[l][j] = matrix[r][j];
+                matrix[r][j] = temp;
+            }
+            l++;
+            r--;
+        }
+        
+        
+        // for(int i = 0; i<n/2;i++){
+        //     for(int j = 0; j<n; j++){
+        //         int temp = matrix[i][j];
+        //         matrix[i][j] = matrix[j][i];
+        //         matrix[j][i] = temp;
+        //     }
+        // }
+        
+        
+        //method second using extra space take o(n^2) space
+    //   int l = 0;
+    //   int k = 0;
+    //   int temp[][] = new int[n][n];
+    //     for(int i = n-1; i>=0; i--){
+    //         //k = 0; 
+        
+    //         for(int j = 0; j<n; j++){
+    //             //k = j;
+    //             //matrix[k][j] = matrix[j][i];
+    //               matrix[l][j]= matrix[j][i];
+    //         }
+    //         l++;
+    //     }
+        
+    //  for(int i = 0; i<n; i++){
+    //      for(int j= 0; j<n; j++){
+    //          matrix[i][j] = matrix[i][j];
+    //      }
+    //  }
+    
     }
 }
