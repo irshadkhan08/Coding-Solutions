d27ab4924f3ae4bff40472d026f8ebef3a34a75a
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-05-09 16:34:03 +0530

    Added solution - LeetHub

--- Insert in Middle of Linked List - GFG/insert-in-middle-of-linked-list.java
+++ Insert in Middle of Linked List - GFG/insert-in-middle-of-linked-list.java
@@ -65,25 +65,58 @@ class Solution {
     public Node insertInMid(Node head, int data){
         //Insert code here, return the head of modified linked list
         
+       //first method
        
+    //   int count = 0;
+    //   Node temp1 = head;
+    //   while(temp1!=null){
+    //       count++;
+    //       temp1 = temp1.next;
+    //   }
        
+    //   Node insert = new Node(data);
+    //   if(head==null){
+    //       return insert;
+    //   }
+    //   //int mid = count/2;
        
-       
-       
+    //   Node temp = head;
+    //   if(count%2==0){
+    //   int i = 0;
+    //   int mid = count/2;
+    //   while(i<mid-1){
+    //       temp = temp.next;
+    //       i++;
+    //   }
+    //   insert.next = temp.next;
+    //   temp.next = insert;
+    //   return head;
+    //   }
+    //   else{
+     
+     
+    //   int i = 0;
+    //   int mid = count/2;
+    //   while(i<mid){
+    //       temp = temp.next;
+    //       i++;
+    //   }
+    //   insert.next = temp.next;
+    //   temp.next = insert;
+    //   return head;
+    //   }
         
+        //second method
         Node slow = head;
         Node fast = head;
           Node key = new Node(data);
           if(head==null){
               return key;
           }
-        
         while(fast.next!=null && fast.next.next!=null){
             slow = slow.next;
             fast = fast.next.next;
         }
-      
-        
         key.next = slow.next;
         slow.next = key;
         return head;
