7080f906b00e50f35770a8508dd5969a88597504
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-05-22 01:00:48 +0530

    Added solution - LeetHub

--- Count the Zeros - GFG/count-the-zeros.java
+++ Count the Zeros - GFG/count-the-zeros.java
@@ -33,14 +33,25 @@ public class Main {
 
 class Solution {
     int countZeroes(int[] arr, int n) {
-        // code here
-        int count = 0;
-        for(int i = 0; i<n; i++){
-            if(arr[i]==0){
-                count++;
-            }
-        }
-        return count;
+      
+      
+      int l = 0;
+      int r = n-1;
+      int ans = 0;
+    
+      
+      while(l<=r){
+          int mid = (l+r)/2;
+          
+          if(arr[mid]==0){
+              ans = n-mid;
+              r = mid-1;
+          }
+          else{
+              l = mid+1;
+          }
+      }
+     return ans;
     }
 
 }
