7a970fbdea8d921f116c6c6cffcff01b537607aa
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-05-23 01:26:06 +0530

    Added solution - LeetHub

---
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            int n = Integer.parseInt(stt.nextToken());
+            int m = Integer.parseInt(stt.nextToken());
+            int k = Integer.parseInt(stt.nextToken());
+            int a[] = new int[(int)(n)];
+            int b[] = new int[(int)(m)];
+            
+            
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < m; i++) {
+                b[i] = Integer.parseInt(inputLine1[i]);
+            }
+            
+            
+            Solution obj = new Solution();
+            System.out.println(obj.kthElement( a, b, n, m, k));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    public long kthElement( int arr1[], int arr2[], int n, int m, int K) {
+          int temp[] = new int[n+m];
+        int k = 0;
+        for(int i = 0; i<n; i++){
+            temp[k++] = arr1[i];
+        }
+         for(int i = 0; i<m; i++){
+            temp[k++] = arr2[i];
+        }
+        
+        Arrays.sort(temp);
+        int n1 = temp.length;
+        
+        int count = 0;
+        for(int i = 0; i<n1; i++){
+            if(i==K-1){
+                count = temp[i];
+            }
+        }
+        return count;
+    }
+}
