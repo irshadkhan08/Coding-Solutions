7ebb58a88f9b0032747accf751ac8857fbca4e4a
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-05-27 00:34:00 +0530

    Added solution - LeetHub

---
+++ Xoring and Clearing - Java - GFG/xoring-and-clearing-java.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    
+	public static void main (String[] args)
+	{
+    	 Scanner sc = new Scanner(System.in);
+    	 int t = sc.nextInt();
+    	
+    	 boolean flag = false;
+    	 while(t-- > 0){
+    	     int n=sc.nextInt();
+    	     int arr[]=new int[n];
+    	     for(int i=0;i<n;i++)
+    	     arr[i]=sc.nextInt();
+    	     
+    	     
+    	        Geeks obj=new Geeks();
+            	obj.xor1ToN(arr,n);
+        	    obj.printArr(arr,n);
+        	    obj.setToZero(arr,n);
+        	    obj.printArr(arr,n);
+    	   
+    	 }
+    }
+}
+
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Geeks{
+    
+    static void printArr(int arr[],int n)
+    {
+        //Your code for printing arr here
+        
+        System.out.println();
+    }
+    
+   static void setToZero(int arr[],int n)
+    {
+        //use Arrays.fill to set arr to zero
+        for(int i= 0; i<n; i++){
+            arr[i]=0;
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+   static void xor1ToN(int arr[], int n)
+    {
+        
+        
+           //Xor arr[i]^i
+           for(int i= 0; i<n;i++){
+               System.out.print((arr[i]^i)+" ");
+           }
+        
+    }
+}
+
+//{ Driver Code Starts.
+
+// } Driver Code Ends
