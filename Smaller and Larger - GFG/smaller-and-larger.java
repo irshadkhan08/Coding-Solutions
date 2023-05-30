30c2b668a33b11e061651785078962ff059d7d45
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-05-31 04:52:18 +0530

    Added solution - LeetHub

---
+++ Smaller and Larger - GFG/smaller-and-larger.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans[] = new Solve().getMoreAndLess(arr, n, x);
+            System.out.println(ans[0] + " " + ans[1]);
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+
+class Solve {
+    int[] getMoreAndLess(int[] arr, int n, int x) {
+        // code here
+        int ans[] = new int[2];
+        int last = 0;
+        int first = 0;
+        for(int i = 0; i<n; i++){
+            if(x>=arr[i]){
+              first++;
+            }
+        }
+        for(int i = 0; i<n; i++){
+            if(x<=arr[i]){
+              last++;
+            }
+        }
+        ans[0] = first;
+        ans[1] = last;
+        return ans;
+    }
+}
