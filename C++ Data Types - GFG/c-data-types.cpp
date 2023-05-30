3ad284d784f4d611ef6699011bf1dd8a7a9d5b92
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-05-30 23:46:13 +0530

    Added solution - LeetHub

---
+++ C++ Data Types - GFG/c-data-types.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+
+    int cppIntType() {
+        // code here
+        int x;
+        cin>>x;
+        return x;
+    }
+    
+    char cppCharType() {
+        // code here
+        char ch;
+        cin>>ch;
+        return ch;
+    }
+    
+    float cppFloatType() {
+        // code here
+        float f;
+        cin>>f;
+        return f;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        Solution ob;
+        cout << ob.cppIntType() << endl;
+        cout << ob.cppCharType() << endl;
+        cout << ob.cppFloatType() << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
