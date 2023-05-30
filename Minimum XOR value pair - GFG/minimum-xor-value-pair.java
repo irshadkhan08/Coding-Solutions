b32bea90f8e58578840bbdf05eaf9f2fb2bf523d
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-05-30 19:01:49 +0530

    Added solution - LeetHub

---
+++ Minimum XOR value pair - GFG/minimum-xor-value-pair.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                arr[i] = Integer.parseInt(input_line[i]);
+            
+
+            Solution ob = new Solution();
+            
+            System.out.println(ob.minxorpair(N,arr));
+            
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+
+    static int minxorpair(int N, int arr[]){
+        // code here
+       
+        int min = Integer.MAX_VALUE;
+        Arrays.sort(arr);
+        
+        for(int i= 0; i<N-1; i++){
+            
+            if((arr[i]^arr[i+1])<min){
+                min = arr[i]^arr[i+1];
+            }
+        }
+        return min;
+    }
+}
