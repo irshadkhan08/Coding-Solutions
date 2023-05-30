8688b6558fcd6733465837fd24a3ca17d79f15cc
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-05-30 21:11:31 +0530

    Added solution - LeetHub

---
+++ Squares in N*N Chessboard - GFG/squares-in-nn-chessboard.java
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            Long N = Long.parseLong(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.squaresInChessBoard(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static Long squaresInChessBoard(Long N) {
+        // code here
+    long ans = 0;
+    if(N==1){
+        return N;
+    }
+    return N*N+squaresInChessBoard(N-1);
+    }
+};
