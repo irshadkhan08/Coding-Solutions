7945b56bbb643db42e6d91cbe07a6494684e412e
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-06-03 00:26:35 +0530

    Added solution - LeetHub

---
+++ Krishnamurthy number - GFG/krishnamurthy-number.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.isKrishnamurthy(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static String isKrishnamurthy(int N) {
+        // code here
+        
+        int n = N;
+        int ans = 0;
+        
+        while(n!=0){
+            int dig = n%10;
+            int f = fact(dig);
+            ans = ans+f;
+            n = n/10;
+        }
+        if(ans==N){
+            return "YES";
+        }
+        else{
+            return "NO";
+        }
+    }
+    public static int fact(int n){
+        if(n==0){
+            return 1;
+        }
+        return n*fact(n-1);
+    }
+};
