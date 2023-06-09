14a9f73747ae8d0b9e76346b55e06008284c8158
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-06-10 01:50:12 +0530

    Added solution - LeetHub

--- Sum Array Puzzle - GFG/sum-array-puzzle.java
+++ Sum Array Puzzle - GFG/sum-array-puzzle.java
@@ -40,34 +40,34 @@ class Geeks
     {
         // your code here
         
-        int sum = 0;
+        // int sum = 0;
         
-        for(int i = 0; i<n; i++){
-            sum = sum+arr[i];
-        }
-        
-        for(int i = 0; i<n; i++){
-            arr[i] = sum-arr[i];
-            System.out.print(arr[i]+" ");
-        }
-        
-        
-        // int ans[] = new int[n];
-        // int k = 0;
         // for(int i = 0; i<n; i++){
-        //     int sum = 0;
-        //     for(int j = 0; j<n; j++){
-        //         if(i==j){
-        //             continue;
-        //         }
-        //         else{
-        //         sum = sum+arr[j];
-        //         }
-        //     }
-        //     ans[k++] = sum;
+        //     sum = sum+arr[i];
         // }
-        // for(int l = 0; l<n; l++){
-        //     arr[l] = ans[l];
+        
+        // for(int i = 0; i<n; i++){
+        //     arr[i] = sum-arr[i];
+        //     System.out.print(arr[i]+" ");
         // }
+        
+        
+        int ans[] = new int[n];
+        int k = 0;
+        for(int i = 0; i<n; i++){
+            int sum = 0;
+            for(int j = 0; j<n; j++){
+                if(i==j){
+                    continue;
+                }
+                else{
+                sum = sum+arr[j];
+                }
+            }
+            ans[k++] = sum;
+        }
+        for(int l = 0; l<n; l++){
+            System.out.print(ans[l]+" ");
+        }
     }
 }
