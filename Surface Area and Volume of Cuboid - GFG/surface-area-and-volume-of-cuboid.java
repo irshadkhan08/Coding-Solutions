bd43e9eac37768a52d1459328d3bdca7c5a03201
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-06-22 04:18:03 +0530

    Added solution - LeetHub

---
+++ Surface Area and Volume of Cuboid - GFG/surface-area-and-volume-of-cuboid.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String s = br.readLine().trim();
+            String S[] = s.split(" ");
+            int l = Integer.parseInt(S[0]);
+            int b = Integer.parseInt(S[1]);
+            int h = Integer.parseInt(S[2]);
+            Solution ob = new Solution();
+            long[] ans = ob.find(l, b, h);
+            for(int i = 0; i < ans.length; i++)System.out.print(ans[i] + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long[] find(int l, int b, int h)
+    {
+        // code here
+        
+        long L = l;
+        long B = b;
+        long H = h;
+        long ans[] = new long[2];
+        
+        ans[0] = 2*(B*H+H*L+L*B);
+        ans[1] = L*B*H;
+        
+        return ans;
+        
+        
+    }
+}
