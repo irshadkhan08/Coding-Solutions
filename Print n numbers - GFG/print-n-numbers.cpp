e27cdda6d8a477b759d01aa6e5dddbbd8695a49b
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-07-01 14:36:37 +0530

    Added solution - LeetHub

---
+++ Print n numbers - GFG/print-n-numbers.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//This Problem has been asked in Amazon, Paytm, Amdocs
+//So Don't Loose hope Keep Trying by 
+//  Post on LinkedIn
+/*
+                #PlacementEdition
+                
+    Get Gifts on Maximum Reach Everyday
+*/
+
+class Solution {
+  public:
+    void printN(int n) {
+        // code here
+       
+        for(int i = 1; i<=n; i++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+   
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        if(n>100000){
+                cout<<"You are excedding limit";
+                return 0;
+        }
+        Solution obj;
+        
+        obj.printN(n);
+        
+    }
+}
+
+// } Driver Code Ends
