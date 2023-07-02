7d6398a3f4a30999e5787ca3911b7275a3a8171a
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-07-02 19:18:28 +0530

    Added solution - LeetHub

---
+++ Check if two given circles touch each other - GFG/check-if-two-given-circles-touch-each-other.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String[] S = read.readLine().split(" ");
+            int X1 = Integer.parseInt(S[0]);
+            int Y1 = Integer.parseInt(S[1]);
+            int R1 = Integer.parseInt(S[2]);
+            int X2 = Integer.parseInt(S[3]);
+            int Y2 = Integer.parseInt(S[4]);
+            int R2 = Integer.parseInt(S[5]);
+            Solution ob = new Solution();
+            System.out.println(ob.circleTouch(X1, Y1, R1, X2, Y2, R2));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function template for Java
+class Solution {
+    int circleTouch(int x1, int y1, int r1, int x2, int y2, int r2) {
+        //Write your code here
+        
+        int c1c2 = (int)Math.abs((int)Math.pow((x1-x2),2)+(int)Math.pow((y1-y2),2));
+        int sum_r1r2 = (r1+r2);
+        sum_r1r2 = (int)Math.pow(sum_r1r2,2);
+        
+        if(c1c2<=sum_r1r2){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+}
