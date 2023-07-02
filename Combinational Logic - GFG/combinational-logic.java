584485a126b2a253699d353544b23e89e18d61b2
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-07-02 21:34:55 +0530

    Added solution - LeetHub

--- Combinational Logic - GFG/combinational-logic.java
+++ Combinational Logic - GFG/combinational-logic.java
@@ -29,8 +29,7 @@ class GFG {
 class Solution {
     static int logicalOperation(int A,int B,int C,int D,int E,int F){
         // code here
-      
-        
-        return (~(A))&B|C&D|E&((~F));
+
+        return ((~(A))&B)|(C&D)|(E&((~F)));
     }
 }
