5ce45f5976f21370ec57feaebe83dc61f1907792
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-07-02 18:28:40 +0530

    Added solution - LeetHub

--- Remove vowels from string - GFG/remove-vowels-from-string.java
+++ Remove vowels from string - GFG/remove-vowels-from-string.java
@@ -27,16 +27,37 @@ class GFG {
 class Solution {
     String removeVowels(String S) {
         // code here
-        StringBuilder res = new StringBuilder();
-        for(int i = 0; i<S.length(); i++){
+        
+        
+        //this is first method : 01
+        // StringBuilder res = new StringBuilder();
+        // for(int i = 0; i<S.length(); i++){
             
-            if((S.charAt(i)=='a'||S.charAt(i)=='e'||S.charAt(i)=='i'||S.charAt(i)=='o'||S.charAt(i)=='u')){
-               continue;
-            }
-            else{
-                res.append(S.charAt(i));
+        //     if((S.charAt(i)=='a'||S.charAt(i)=='e'||S.charAt(i)=='i'||S.charAt(i)=='o'||S.charAt(i)=='u')){
+        //       continue;
+        //     }
+        //     else{
+        //         res.append(S.charAt(i));
+        //     }
+        // }
+        // return res.toString();
+        
+     
+        StringBuffer sb = new StringBuffer();
+        
+        for(int i = 0; i<S.length(); i++){
+            char ch = S.charAt(i);
+            
+            if(!vowel(ch)){
+                sb.append(ch);
             }
         }
-        return res.toString();
+        return sb.toString();
+    }
+    public static boolean vowel(char ch){
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+            return true;
+        }
+        return false;
     }
 }
