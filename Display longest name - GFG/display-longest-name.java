604ff1def272496f3b8c152840b2df0d7a4e563a
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-07-03 21:27:00 +0530

    Added solution - LeetHub

--- Display longest name - GFG/display-longest-name.java
+++ Display longest name - GFG/display-longest-name.java
@@ -29,20 +29,35 @@ import java.util.Scanner;
 
 class GFG {
     String longest(String arr[], int n) {
-        int max = Integer.MIN_VALUE;
-        String str = "";
-        for(int i = 0; i<n; i++){
-            int l = arr[i].length();
-            max = Math.max(max,l);
-        }
         
-        for(int i = 0; i<n; i++){
-            int l = arr[i].length();
-            if(l==max){
-                str = str+arr[i];
-                break;
+        // this is first method : 01
+        // int max = Integer.MIN_VALUE;
+        // String str = "";
+        // for(int i = 0; i<n; i++){
+        //     int l = arr[i].length();
+        //     max = Math.max(max,l);
+        // }
+        
+        // for(int i = 0; i<n; i++){
+        //     int l = arr[i].length();
+        //     if(l==max){
+        //         str = str+arr[i];
+        //         break;
+        //     }
+        // }
+        // return str;
+        
+        
+        // this is second method : 02
+        
+        String str = "";
+        int max = 0;
+        
+        for(int i =0; i<n; i++){
+            if(arr[i].length()>arr[max].length()){
+                max = i;
             }
         }
-        return str;
+        return arr[max];
     }
 }
