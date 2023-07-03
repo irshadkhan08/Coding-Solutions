544eeead4143754279c0a4128579cf0a9022f031
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-07-04 03:01:05 +0530

    Added solution - LeetHub

--- Count the subarrays having product less than k - GFG/count-the-subarrays-having-product-less-than-k.java
+++ Count the subarrays having product less than k - GFG/count-the-subarrays-having-product-less-than-k.java
@@ -52,16 +52,55 @@ class Solution {
     
     public int countSubArrayProductLessThanK(long a[], int n, long k)
     {
+    //     int count = 0;
+    //   // Arrays.sort(a);
+    //     for(int i = 0; i<n; i++){
+    //         long mul = 1;
+    //         for(int j= i; j<n; j++){
+             
+    //             if(mul*a[j]<k){
+    //                 mul = a[j]*mul;
+    //                 count = count+1;
+                   
+    //             }
+    //             else{
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     return count;
+    //int count = 0;
+        //Arrays.sort(a);
+        // for(int i = 0; i<n; i++){
+        //     long mul = a[i];
+        //     if(mul<k){
+        //         count++;
+        //     }
+            
+        //     for(int j= i+1; j<n; j++){
+        //       mul = mul*a[j];
+        //         if(mul<k){
+        //             count = count+1;
+                    
+        //         }
+        //         else{
+        //             break;
+        //         }
+        //     }
+        // }
+        // return count;
+        
+        
+        
+        
         int count = 0;
-       // Arrays.sort(a);
+        
         for(int i = 0; i<n; i++){
             long mul = 1;
             for(int j= i; j<n; j++){
-             
-                if(mul*a[j]<k){
-                    mul = a[j]*mul;
-                    count = count+1;
-                   
+                mul = mul*a[j];
+                if(mul<k){
+                    count++;
                 }
                 else{
                     break;
