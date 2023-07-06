4596a5126d6457d6e358f2a490985f68fe034838
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-07-06 10:17:39 +0530

    Added solution - LeetHub

--- Length of Last word - GFG/length-of-last-word.java
+++ Length of Last word - GFG/length-of-last-word.java
@@ -24,28 +24,46 @@ class Solution
     { 
         // code here
        
-       int l = 0;
        
-       char ch1 = s.charAt(s.length()-1);
-       if(ch1==32){
-           l = 0 ;
-       }
-       else{
-           l = l+1;
-       }
+       // This is first method :01
+       
+    //   int l = 0;
+       
+    //   char ch1 = s.charAt(s.length()-1);
+    //   if(ch1==32){
+    //       l = 0 ;
+    //   }
+    //   else{
+    //       l = l+1;
+    //   }
     
-       for(int i = s.length()-2; i>=0; i--){
-           char ch = s.charAt(i);
+    //   for(int i = s.length()-2; i>=0; i--){
+    //       char ch = s.charAt(i);
            
-           if((ch>=65 && ch<=97)||(ch>=97 && ch<=122)){
-               l++;
-           }
-           else{
-               break;
-           }
-       }
+    //       if((ch>=65 && ch<=97)||(ch>=97 && ch<=122)){
+    //           l++;
+    //       }
+    //       else{
+    //           break;
+    //       }
+    //   }
 
-       return l;
+    //   return l;
+    
+    
+    // This is second method : 02
+    
+    int l = 0;
+    String trim = s.trim();
+    char ch[] = trim.toCharArray();
+    for(int i = ch.length-1;i>=0; i-- ){
+        l++;
         
+        if(ch[i]==32){
+            return l-1;
+        }
+    }
+    return l;
+    
     }
 }
