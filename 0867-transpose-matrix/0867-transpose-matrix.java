063a3fb80fc207b2eaa86e2f4590d1b0a846c6ee
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-07-15 03:15:51 +0530

    Time: 0 ms (100.00%), Space: 44.9 MB (9.00%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.java
+++ 0867-transpose-matrix/0867-transpose-matrix.java
@@ -3,10 +3,19 @@ class Solution {
         int row = arr.length;
         int col = arr[0].length;
       
-        int res[][] = new int[col][row];
-        for(int i = 0; i<col; i++){
-            for(int j = 0; j<row; j++){
-               res[i][j] = arr[j][i];
+        // int res[][] = new int[col][row];
+        // for(int i = 0; i<col; i++){
+        //     for(int j = 0; j<row; j++){
+        //        res[i][j] = arr[j][i];
+        //     }
+        // }
+        // return res;
+        
+        
+        int res[][] = new int[arr[0].length][arr.length];
+        for(int i = 0; i<arr.length; i++){
+            for(int j = 0; j<arr[0].length; j++){
+                res[j][i] = arr[i][j];
             }
         }
         return res;
