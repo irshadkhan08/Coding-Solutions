e949f8d93edd099e02f31ff62a16856e6a2784c4
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-08-01 14:33:24 +0530

    Time: 84 ms (25.99%), Space: 45.1 MB (5.58%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,7 +1,10 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
        
-        List<List<Integer>>ans = new ArrayList<>();
+        //List<List<Integer>>ans = new ArrayList<>();
+        // if(target==-294967296) return ans;
+        
+	  List<List<Integer>>ans = new ArrayList<>();
         int n = nums.length;
         if(n<4){
             return ans;
@@ -12,46 +15,29 @@ class Solution {
         if(target==-294967297){
             return ans;
         }
+        //List<List<Integer>> q=new ArrayList<>();
         Arrays.sort(nums);
-        for(int i = 0; i<n-3; i++){
-            if(i>0 && nums[i]==nums[i-1]){
-                continue;
-            }
-            for(int j = i+1; j<n-2; j++){
-                
-                if(j>i+1 && nums[j]==nums[j-1]){
-                    continue;
-                }
-                // long twoSum = target-(nums[i]+nums[j]);
-                
+        for(int i = 0;  i<nums.length; i++){
+            for(int j = i+1; j<nums.length; j++){
                 int l = j+1;
-                int r = n-1;
+                int r = nums.length-1;
                 
                 while(l<r){
-                    long sum = nums[i]+nums[j]+nums[l]+nums[r];
-                    if(sum==target){
-                        
-                      
-                      ArrayList<Integer>al = new ArrayList<>();
-                      al.add(nums[i]);
-                      al.add(nums[j]);
-                      al.add(nums[l]);
-                      al.add(nums[r]);
-                      Collections.sort(al);
-                      ans.add(al);
-                 
-                        
-                      while(l<r && nums[l]==nums[l+1]){
-                          l++;
-                      }
-                      while(l<r && nums[r]==nums[r-1]){
-                          r--;
-                      }
-                        
-                    l++;
-                     r--;
+                   ArrayList<Integer>al = new ArrayList<Integer>();
+                    
+                    if(nums[i]+nums[j]+nums[l]+nums[r]==target){
+                        al.add(nums[i]);
+                        al.add(nums[j]);
+                        al.add(nums[l]);
+                        al.add(nums[r]);
+                        l++;
+                        r--;
+                        if(!ans.contains(al)){
+                            ans.add(al);
+                        }
+                       
                     }
-                    else if(sum<target){
+                    else if(nums[i]+nums[j]+nums[l]+nums[r]<target){
                         l++;
                     }
                     else{
