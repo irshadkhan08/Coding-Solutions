3923abe8d242b94e216d52368f1af30a566ae6ea
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-08-22 11:27:17 +0530

    Added solution - LeetHub

---
+++ Pattern Searching - GFG/pattern-searching.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String input[] = read.readLine().split(" ");
+            String text = input[0];
+            String pattern = input[1];
+            Solution ob = new Solution();
+            System.out.println(ob.search(text,pattern));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int search(String text, String pat) {
+        // code here
+        if(text.contains(pat)){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+};
