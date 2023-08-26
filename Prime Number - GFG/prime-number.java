3c02153c757fda95870d54bbe01d71595990d75f
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-08-27 01:33:53 +0530

    Added solution - LeetHub

--- Prime Number - GFG/prime-number.java
+++ Prime Number - GFG/prime-number.java
@@ -21,11 +21,25 @@ class GFG
 
 class Solution{
     static int isPrime(int N){
-        // code here
-        if(N==1){
-            return 0;
-        }
-        for(int i = 2; i<=Math.sqrt(N); i++){
+        // // code here
+        // if(N==1){
+        //     return 0;
+        // }
+        // for(int i = 2; i<=Math.sqrt(N); i++){
+        //     if(N%i==0){
+        //         return 0;
+        //     }
+        // }
+        // return 1;
+        
+        
+        
+        //efficient rule
+        
+        if(N==2)return 1;
+        if(N==1||N%2==0)return 0;
+        
+        for(int i = 3; i*i<=N; i+=2){
             if(N%i==0){
                 return 0;
             }
