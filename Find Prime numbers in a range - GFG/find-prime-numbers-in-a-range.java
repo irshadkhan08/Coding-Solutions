522aecef020dd64039ca2e3072c7379b63b47068
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-08-28 05:00:26 +0530

    Added solution - LeetHub

--- Find Prime numbers in a range - GFG/find-prime-numbers-in-a-range.java
+++ Find Prime numbers in a range - GFG/find-prime-numbers-in-a-range.java
@@ -41,9 +41,9 @@ class Solution {
         return ans;
     }
     public static boolean isPrime(int n){
-        
-      
-        for(int i = 2; i<=(int)Math.sqrt(n); i++){
+        if(n==2)return true;
+        if(n%2==0)return false;
+        for(int i = 3; i<=(int)Math.sqrt(n); i++){
             if(n%i==0){
                 return false;
             }
