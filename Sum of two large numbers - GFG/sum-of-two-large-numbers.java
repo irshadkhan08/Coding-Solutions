90f2561f6e95fc4d8ad083ad62ae5e0698be3825
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-08-30 10:07:42 +0530

    Added solution - LeetHub

--- Sum of two large numbers - GFG/sum-of-two-large-numbers.java
+++ Sum of two large numbers - GFG/sum-of-two-large-numbers.java
@@ -25,11 +25,60 @@ class GFG {
 // User function Template for Java
 
 class Solution {
-    String findSum(String X, String Y) {
+    String findSum(String s1, String s2) {
         // code here
-        java.math.BigInteger a = new java.math.BigInteger(X);
-       java.math.BigInteger b = new java.math.BigInteger(Y);
-       a = a.add(b);
-       return a.toString();
+       
+      
+        StringBuilder sum = new StringBuilder();
+      int carry = 0;
+ 
+      int i = s1.length()-1;
+      int j = s2.length()-1;
+     
+      while(i>=0 || j>=0){
+          int  d = 0;
+          if(i>=0){
+              d+=s1.charAt(i)-'0';
+              i--;
+          }
+          if(j>=0){
+            d+=s2.charAt(j)-'0';
+            j--;
+          }
+           d = d+carry;
+           sum.append(d%10);
+           carry = d/10;
+            
+          
+      }
+      
+    if(carry==1){
+        sum.append("1");
+     }
+//       String str=sum.reverse().toString();
+//       int idx=0;
+//       while(idx< str.length()-1 && str.charAt(idx)=='0'){
+//               idx++;
+//       }
+
+// return str.substring(idx);
+
+     
+      
+   
+      String ans =sum.reverse().toString();
+      
+    //  sum.reverse();
+      
+      int idx = 0;
+      while(idx<ans.length()-1 && ans.charAt(idx)=='0'){
+          idx++;
+      }
+      if(idx==ans.length()){
+          return "0";
+        
+      }
+      return ans.substring(idx);
+
     }
 }
