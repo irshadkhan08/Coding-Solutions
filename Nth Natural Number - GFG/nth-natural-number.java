44906deaa04a01aefeee2a216f4a9b8f9225a818
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-09-07 05:10:32 +0530

    Added solution - LeetHub

---
+++ Nth Natural Number - GFG/nth-natural-number.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner ob=new Scanner(System.in);
+		int t=ob.nextInt();
+		while(t-->0)
+		{
+		    long n=ob.nextLong();
+		    Solution ab=new Solution();
+		    long k=ab.findNth(n);
+		    System.out.println(k);
+		}
+	}
+}
+
+    
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    long findNth(long N)
+    {
+        //code here
+        String ans = "";
+        while(N!=0){
+            ans = (N%9)+ans;
+            N = N/9;
+        }
+        return Long.parseLong(ans);
+    }
+}
