418c0a58927a716c0cf9941513d2a8880f394c0b
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-09-09 02:45:05 +0530

    Added solution - LeetHub

---
+++ Convert an array to reduced form - GFG/convert-an-array-to-reduced-form.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().convert(arr, n);
+            for (int i = 0; i < n; i++) {
+                out.print(arr[i] + " ");
+            }
+            out.println();
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    void convert(int[] arr, int n) {
+        //Code here
+        
+        int copy[] = new int[n];
+        int k = 0;
+        for(int i = 0; i<n; i++){
+            copy[k++] = arr[i];
+        }
+        
+        Arrays.sort(copy);
+        
+        HashMap<Integer,Integer>map = new HashMap<>();
+        
+        for(int i = 0; i<n; i++){
+            map.put(copy[i],i);
+        }
+        
+        for(int i = 0; i<n; i++){
+            arr[i] = map.get(arr[i]);
+        }
+    }
+}
