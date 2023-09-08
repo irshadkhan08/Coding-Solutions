ca93741849fde58afd095ab8561dbc30623eb265
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-09-09 04:52:13 +0530

    Added solution - LeetHub

--- Smallest distinct window - GFG/smallest-distinct-window.java
+++ Smallest distinct window - GFG/smallest-distinct-window.java
@@ -30,25 +30,28 @@ class GFG {
 class Solution {
     public int findSubString( String str) {
         // Your code goes here
+          
+       
         HashSet<Character>set = new HashSet<>();
         int n = str.length();
         for(int i = 0; i<n; i++){
             set.add(str.charAt(i));
         }
-        
         int k = set.size();
-        
         char arr[] = str.toCharArray();
-        HashMap<Character,Integer>map = new HashMap<>();
+         int map[] = new int[256];
         int i = 0;
         int j = 0;
         int ans = Integer.MAX_VALUE;
+        int mapsize = 0;
         while(j<n){
-            map.put(arr[j],map.getOrDefault(arr[j],0)+1);
-            
-            if(map.size()==k){
-                while(map.get(arr[i])>1){
-                       map.put(arr[i],map.getOrDefault(arr[i],0)-1);
+            map[str.charAt(j)]++;
+            if(map[str.charAt(j)]==1){
+                mapsize++;
+            }
+            if(mapsize==k){
+                while(map[str.charAt(i)]>1){
+                      map[str.charAt(i)]--;
                        i++;
                 }
                 ans = Math.min(ans,j-i+1);
@@ -56,5 +59,35 @@ class Solution {
             j++;
         }
         return ans;
+          
+          
+
+          
+        // HashSet<Character>set = new HashSet<>();
+        // int n = str.length();
+        // for(int i = 0; i<n; i++){
+        //     set.add(str.charAt(i));
+        // }
+        
+        // int k = set.size();
+        
+        // char arr[] = str.toCharArray();
+        // HashMap<Character,Integer>map = new HashMap<>();
+        // int i = 0;
+        // int j = 0;
+        // int ans = Integer.MAX_VALUE;
+        // while(j<n){
+        //     map.put(arr[j],map.getOrDefault(arr[j],0)+1);
+            
+        //     if(map.size()==k){
+        //         while(map.get(arr[i])>1){
+        //               map.put(arr[i],map.getOrDefault(arr[i],0)-1);
+        //               i++;
+        //         }
+        //         ans = Math.min(ans,j-i+1);
+        //     }
+        //     j++;
+        // }
+        // return ans;
     }
 }
