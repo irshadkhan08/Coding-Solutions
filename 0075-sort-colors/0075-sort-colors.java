c2e06cacea59ad41acc25f8ee644265ee6a7c44d
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-09-10 01:29:59 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (25.77%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,29 +1,59 @@
 class Solution {
     public void sortColors(int[] arr) {
-        int zero = 0;
-        int one = 0;
-        int two = 0;
+        
+        
+        //datch flag algorithms
         int n = arr.length;
-        for(int i = 0; i<n; i++){
-            if(arr[i]==0){
-                zero++;
+        int low = 0;
+        int mid = 0;
+        int high = n-1;
+        
+        while(mid<=high){
+            if(arr[mid]==0){
+                int temp =arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                mid++;
+                low++;
             }
-            else if(arr[i]==1){
-                one++;
+            else if(arr[mid]==1){
+                mid++;
             }
             else{
-                two++;
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
             }
         }
         
-        for(int i = 0; i<zero; i++){
-            arr[i]=0;
-        }
-        for(int i = zero; i<zero+one; i++){
-            arr[i]=1;
-        }
-         for(int i = zero+one; i<n; i++){
-            arr[i]=2;
-        }
+        
+        
+        
+//         int zero = 0;
+//         int one = 0;
+//         int two = 0;
+//         int n = arr.length;
+//         for(int i = 0; i<n; i++){
+//             if(arr[i]==0){
+//                 zero++;
+//             }
+//             else if(arr[i]==1){
+//                 one++;
+//             }
+//             else{
+//                 two++;
+//             }
+//         }
+        
+//         for(int i = 0; i<zero; i++){
+//             arr[i]=0;
+//         }
+//         for(int i = zero; i<zero+one; i++){
+//             arr[i]=1;
+//         }
+//          for(int i = zero+one; i<n; i++){
+//             arr[i]=2;
+//         }
     }
 }
