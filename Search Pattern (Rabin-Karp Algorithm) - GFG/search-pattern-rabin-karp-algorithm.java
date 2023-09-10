1ca33ed0280be902fc8797f357751fbfd4126805
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-09-10 23:53:35 +0530

    Added solution - LeetHub

--- Search Pattern (Rabin-Karp Algorithm) - GFG/search-pattern-rabin-karp-algorithm.java
+++ Search Pattern (Rabin-Karp Algorithm) - GFG/search-pattern-rabin-karp-algorithm.java
@@ -29,32 +29,63 @@ class GFG
 // } Driver Code Ends
 
 
+
+
 //User function Template for Java
 
 class Solution
 {
+    public final static int d=256;
     
-    ArrayList<Integer> search(String pat, String str)
+    ArrayList<Integer> search(String pat, String S)
     {
         // your code here
-        ArrayList<Integer>ans = new ArrayList<>();
-        if(!str.contains(pat)){
-            ans.add(-1);
-            return ans;
+        int q=101;
+        ArrayList<Integer> ar=new ArrayList<>();
+        int M = pat.length();
+        int N = S.length();
+        int i, j;
+        int p = 0; // hash value for pattern
+        int t = 0; // hash value for txt
+        int h = 1;
+ 
+        // The value of h would be "pow(d, M-1)%q"
+        for (i = 0; i < M - 1; i++)
+            h = (h * d) % q;
+ 
+        // Calculate the hash value of pattern and first
+        // window of text
+        for (i = 0; i < M; i++) {
+            p = (d * p + pat.charAt(i)) % q;
+            t = (d * t + S.charAt(i)) % q;
         }
-     int n1 = str.length()-1;
-       int n2 = pat.length()-1;
-       for(int i = 0; i<=n1-n2; i++){
-         int j = 0;
-          for(; j<pat.length(); j++){
-              if(str.charAt(i+j)!=pat.charAt(j)){
-                 break; 
-              }
-          }
-          if(j==pat.length()){
-              ans.add(i+1);
-          }
-       }
-       return ans;
+ 
+        // Slide the pattern over text one by one
+        for (i = 0; i <= N - M; i++) {
+            if (p == t) {
+                for (j = 0; j < M; j++) {
+                    if (S.charAt(i + j) != pat.charAt(j))
+                        break;
+                }
+ 
+                // if p == t and pat[0...M-1] = txt[i, i+1,
+                // ...i+M-1]
+                if (j == M)
+                    ar.add(i+1);
+            }
+ 
+            // Calculate hash value for next window of text:
+            // Remove leading digit, add trailing digit
+            if (i < N - M) {
+                t = (d * (t - S.charAt(i) * h) + S.charAt(i + M)) % q;
+ 
+                if (t < 0)
+                    t = (t + q);
+            }
+        }
+        if(!ar.isEmpty()) return ar;
+        ar.add(-1);
+        return ar;
+
     }
 }
