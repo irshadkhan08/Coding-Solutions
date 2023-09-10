882971c2bc91acbd02e70971355ed4c4f4e90b12
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-09-10 22:06:29 +0530

    Added solution - LeetHub

---
+++ Search Pattern (Rabin-Karp Algorithm) - GFG/search-pattern-rabin-karp-algorithm.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            String s, patt;
+            s = sc.next();
+            patt = sc.next();
+            
+            Solution ob = new Solution();
+            
+            ArrayList<Integer> res = ob.search(patt, s);
+            
+            for(int i = 0;i<res.size();i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println();    
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    
+    ArrayList<Integer> search(String pat, String str)
+    {
+        // your code here
+        ArrayList<Integer>ans = new ArrayList<>();
+        if(str.length()==0||pat.length()==0){
+            ans.add(-1);
+            return ans;
+        }
+        
+        if(!str.contains(pat)){
+            ans.add(-1);
+            return ans;
+        }
+     int n1 = str.length()-1;
+       int n2 = pat.length()-1;
+       for(int i = 0; i<=n1-n2; i++){
+         int j = 0;
+          for(; j<pat.length(); j++){
+              if(str.charAt(i+j)!=pat.charAt(j)){
+                 break; 
+              }
+          }
+          if(j==pat.length()){
+              ans.add(i+1);
+          }
+       }
+       return ans;
+    }
+}
