648e766d37976fbbac334b14d832ac1383837558
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-09-14 00:38:52 +0530

    Added solution - LeetHub

--- Quick Sort - GFG/quick-sort.java
+++ Quick Sort - GFG/quick-sort.java
@@ -36,12 +36,31 @@ class Solution
     //Function to sort an array using quick sort algorithm.
     static void quickSort(int arr[], int low, int high)
     {
-        // code here
-        Arrays.sort(arr);
-    }
-    // static int partition(int arr[], int low, int high)
-    // {
-    //     // your code here
         
-    // } 
+        if(low<high){
+        int partitionIndex = partition(arr,low,high);
+        quickSort(arr,low,partitionIndex-1);
+        quickSort(arr,partitionIndex+1,high);
+       
+        }
+    }
+    static int partition(int arr[], int low, int high)
+    {
+        // your code here
+        
+        int i = low-1;
+        int pivot = arr[high];
+        for(int j = low; j<high;j++ ){
+            if(arr[j]<pivot){
+                i++;
+               int temp = arr[i];
+               arr[i] = arr[j];
+               arr[j] = temp;
+            }
+        }
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+        return i+1;
+    } 
 }
