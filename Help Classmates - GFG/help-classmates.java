6501b4b098f359f068f6bf61bd3fc609fc3bf5b5
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-09-16 11:55:51 +0530

    Added solution - LeetHub

---
+++ Help Classmates - GFG/help-classmates.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*; 
+import java.io.*; 
+
+class GFG 
+{ 
+	public static void main (String[] args) { 
+		Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+            int array[] = new int[n];
+            for (int i = 0; i < n; ++i)
+            {
+                array[i] = sc.nextInt();
+            }
+
+            Solution ob = new Solution();
+
+            int ans[] = ob.help_classmate(array,n);
+
+           	for (int i=0; i<n; i++) 
+                System.out.print(ans[i]+" "); 
+            System.out.println();
+            t--;
+        }
+	} 
+} 
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+	public static int[] help_classmate(int arr[], int n) 
+	{ 
+	    // Your code goes here
+	    ArrayList<Integer>al = new ArrayList<>();
+        int ans[] = new int[n];
+        Stack<Integer>st = new Stack<>();
+        ans[n-1] = -1;
+        st.push(arr[n-1]);
+        for(int i = n-2; i>=0; i--){
+            while(!st.isEmpty() && st.peek()>=arr[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                ans[i] = -1;
+            }
+            else{
+                ans[i] = st.peek();
+            }
+            st.push(arr[i]);
+        }
+      return ans;
+	} 
+}
