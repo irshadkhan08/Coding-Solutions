da342cc4bd219c6c2511011154b7749c7170391f
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-09-16 11:30:48 +0530

    Added solution - LeetHub

--- Next Greater Element - GFG/next-greater-element.java
+++ Next Greater Element - GFG/next-greater-element.java
@@ -9,34 +9,50 @@ class GFG {
     
 	public static void main (String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter ot = new PrintWriter(System.out);
 		int t = Integer.parseInt(br.readLine().trim());
 		while(t-->0){
 		    int n = Integer.parseInt(br.readLine().trim());
 		    String inputLine[] = br.readLine().trim().split(" ");
 		    long[] arr = new long[n];
 		    for(int i=0; i<n; i++)arr[i]=Long.parseLong(inputLine[i]);
-		    Solution ob = new Solution();
-		    long[] res = ob.nextLargerElement(arr, n);
+		    long[] res = new Solution().nextLargerElement(arr, n);
 		    for (int i = 0; i < n; i++) 
-		        ot.print(res[i] + " ");
-		    ot.println();
+		        System.out.print(res[i] + " ");
+		    System.out.println();
 		}
-        ot.close();
 	}
 }
+
 
 
 
 // } Driver Code Ends
 
 
-// User Function Template for JAVA
 
-class Solution{
-    public static long[] nextLargerElement(long[] arr, int n) { 
+
+class Solution
+{
+    //Function to find the next greater element for each element of the array.
+    public static long[] nextLargerElement(long[] arr, int n)
+    { 
         // Your code here
-        ArrayList<Long>al = new ArrayList<>();
+    //     ArrayList<Long>al = new ArrayList<>();
+    //     long ans[] = new long[n];
+    //     Stack<Long>st = new Stack<>();
+    //     ans[n-1] = -1;
+    //     st.push(arr[n-1]);
+    //     for(int i = n-2; i>=0; i--){
+    //         while(!st.isEmpty() && st.peek()<=arr[i]){
+    //             st.pop();
+    //         }
+    //             long nxt = st.isEmpty()?(-1):st.peek();
+    //             ans[i] = nxt;
+    //             st.push(arr[i]);
+    //     }
+    //   return ans;
+    
+    ArrayList<Long>al = new ArrayList<>();
         long ans[] = new long[n];
         Stack<Long>st = new Stack<>();
         ans[n-1] = -1;
@@ -45,9 +61,13 @@ class Solution{
             while(!st.isEmpty() && st.peek()<=arr[i]){
                 st.pop();
             }
-                long nxt = st.isEmpty()?(-1):st.peek();
-                ans[i] = nxt;
-                st.push(arr[i]);
+            if(st.isEmpty()){
+                ans[i] = -1;
+            }
+            else{
+                ans[i] = st.peek();
+            }
+            st.push(arr[i]);
         }
       return ans;
     } 
