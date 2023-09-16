f21919915ea1d27545ed54da57cf9ad471707e87
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-09-16 11:24:42 +0530

    Added solution - LeetHub

--- Next Greater Element - GFG/next-greater-element.java
+++ Next Greater Element - GFG/next-greater-element.java
@@ -9,32 +9,33 @@ class GFG {
     
 	public static void main (String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter ot = new PrintWriter(System.out);
 		int t = Integer.parseInt(br.readLine().trim());
 		while(t-->0){
 		    int n = Integer.parseInt(br.readLine().trim());
 		    String inputLine[] = br.readLine().trim().split(" ");
 		    long[] arr = new long[n];
 		    for(int i=0; i<n; i++)arr[i]=Long.parseLong(inputLine[i]);
-		    Solution ob = new Solution();
-		    long[] res = ob.nextLargerElement(arr, n);
+		    long[] res = new Solution().nextLargerElement(arr, n);
 		    for (int i = 0; i < n; i++) 
-		        ot.print(res[i] + " ");
-		    ot.println();
+		        System.out.print(res[i] + " ");
+		    System.out.println();
 		}
-        ot.close();
 	}
 }
+
 
 
 
 // } Driver Code Ends
 
 
-// User Function Template for JAVA
 
-class Solution{
-    public static long[] nextLargerElement(long[] arr, int n) { 
+
+class Solution
+{
+    //Function to find the next greater element for each element of the array.
+    public static long[] nextLargerElement(long[] arr, int n)
+    { 
         // Your code here
         ArrayList<Long>al = new ArrayList<>();
         long ans[] = new long[n];
