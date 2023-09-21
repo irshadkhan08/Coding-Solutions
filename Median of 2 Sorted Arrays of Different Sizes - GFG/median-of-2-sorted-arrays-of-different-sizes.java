8f4dbb8babc4e9d323f6866a5c2ab5095f3e4bb4
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-09-22 01:48:12 +0530

    Added solution - LeetHub

--- Median of 2 Sorted Arrays of Different Sizes - GFG/median-of-2-sorted-arrays-of-different-sizes.java
+++ Median of 2 Sorted Arrays of Different Sizes - GFG/median-of-2-sorted-arrays-of-different-sizes.java
@@ -35,32 +35,39 @@ class Driver
 
 class GFG 
 { 
-    static double medianOfArrays(int n, int m, int a[], int b[]) 
+    static double medianOfArrays(int n, int m, int a1[], int a2[]) 
     {
         // Your Code Here
-        int temp[] = new int[n+m];
-        
-        int k = 0;
-        for(int i = 0; i<n; i++){
-            temp[k++] = a[i];
+      int n1 = a1.length;
+        int n2 = a2.length;
+        if(n1 > n2) return medianOfArrays(n,m,a2, a1);
+
+        int l = 0, r = n1; // binary search in a1
+        while(l <= r) {
+            int m1 = (l+r)/2;
+            int m2 = (n1+n2+1)/2 - m1;
+
+            // [0...m1-1] left of a1, [m1...n1-1] right of a1
+            // [0...m2-1] left of a2, [m2....n2-1] right of a2
+
+            int max1 = m1 == 0 ? Integer.MIN_VALUE : a1[m1-1];
+            int max2 = m2 == 0 ? Integer.MIN_VALUE : a2[m2-1];
+
+            int min1 = m1 == n1 ? Integer.MAX_VALUE : a1[m1];
+            int min2 = m2 == n2 ? Integer.MAX_VALUE : a2[m2];
+
+            if(max1 <= min2 && max2 <= min1) {
+                if((n1+n2)%2 == 0) {
+                    return ((double)Math.max(max1, max2) + (double)Math.min(min1, min2))/2;
+                } else {
+                    return (double)Math.max(max1, max2);
+                }
+            } else if(max2 > min1) {
+                l = m1+1;
+            } else {
+                r = m1-1;
+            }
         }
-        
-        for(int i = 0; i<m; i++){
-            temp[k++] = b[i];
-        }
-        
-        
-        Arrays.sort(temp);
-        int l = temp.length;
-        double ans = 0;
-        if(l%2==1){
-            ans = (temp[l/2]);
-        }
-        else{
-            ans = temp[l/2]+temp[(l/2)-1];
-            ans = ans/2;
-        }
-        
-        return ans;
+        return 0.0;
     }
 }
