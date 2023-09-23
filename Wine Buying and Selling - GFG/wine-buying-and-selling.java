cd660e8f41ad5cb480b5d61323fe9997521ef45b
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-09-23 18:29:53 +0530

    Added solution - LeetHub

---
+++ Wine Buying and Selling - GFG/wine-buying-and-selling.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T>0)
+        {
+            int N = sc.nextInt();
+            int arr[] = new int[N];
+            for(int i=0; i<N; i++)
+                arr[i] = sc.nextInt();
+            
+            Solution g = new Solution();
+            long ans = g.wineSelling(arr,N);
+            
+            System.out.println(ans);
+            T--;
+        }
+    }
+}
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+class Solution {
+    long wineSelling(int Arr[],int N){
+        // code here
+        
+        long ans[] = new long[N];
+        
+        // ans[0] = Arr[0];
+        int sum = 0;
+        int k = 0;
+        for(int i = 0; i<N; i++){
+            sum = sum+Arr[i];
+            ans[k++] = sum;
+        }
+        
+        long res = 0;
+        
+        for(int i = 0; i<ans.length; i++){
+            res = res+Math.abs(ans[i]);
+        }
+        
+        return res;
+    }
+}
+
+//{ Driver Code Starts.
+// } Driver Code Ends
