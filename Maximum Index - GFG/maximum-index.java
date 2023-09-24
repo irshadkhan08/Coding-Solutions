29669b8ee5eb78a86108b45dce609b4e8f080e1b
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-09-24 14:43:42 +0530

    Added solution - LeetHub

--- Maximum Index - GFG/maximum-index.java
+++ Maximum Index - GFG/maximum-index.java
@@ -2,88 +2,76 @@
 import java.io.*;
 import java.util.*;
 
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-        int t =
-            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
-        while (t-- > 0) {
-            int n = Integer.parseInt(br.readLine().trim());
-            int arr[] = new int[n];
-            String inputLine[] = br.readLine().trim().split(" ");
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(inputLine[i]);
-            }
-            System.out.println(new Solution().maxIndexDiff(arr, n));
-        }
-    }
+class Main {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    int n = Integer.parseInt(br.readLine().trim()); // size of array
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]); // input elements of array
+		    }
+		    
+		    Solution ob = new Solution();
+		    
+		    System.out.println(ob.maxIndexDiff(arr, n)); // print the result
+		}
+	}
 }
 // } Driver Code Ends
 
 
-class Solution {
-
-    int maxIndexDiff(int arr[], int n) {
-        // code here
+class Solution{
     
+    // A[]: input array
+    // N: size of array
+    // Function to find the maximum index difference.
+    static int maxIndexDiff(int A[], int N) { 
         
-        // int ans = 0;
+        // Your code here
         
-        // for(int i = 0; i<n; i++)
-        // {
-        //     for(int j = n-1; j>i; j--){
-        //         if(arr[i]<=arr[j]){
-        //             int temp = j-i;
-        //             ans = Math.max(ans,temp);
-        //             break;
-                   
-        //         }
-        //     }
-        // }
-        // return ans;
-        
-        
-        
-        
-        
-        int left[] = new int[n];
-        int min = Integer.MAX_VALUE;
-        for(int i = 0; i<n;i++){
-            if(arr[i]<min){
-                min  = arr[i];
-            }
-            left[i]=min;
-        }
-        
-        int right[] = new int[n];
-        int max = arr[n-1];
-        
-    
-        for(int i = n-1; i>=0; i--){
-            if(arr[i]>max){
-                max = arr[i];
-            }
-            right[i] = max;
-        }
-        
-        int i = 0;
-        int j = 0;
-        int ans = -1;
-        while(i<n && j<n){
-        
-            if(left[i]<=right[j]){
-              ans =  Math.max(ans,j-i);
-              j++;
-            }
-            else{
-                i++;
-            }
-        }
-  
-        return ans;
-        
-         
-
+       int left[] = new int[N];
+       int min = A[0];
+       
+       for(int i = 0; i<N; i++)
+       {
+           if(min>A[i]){
+               min = A[i];
+           }
+           left[i] = min;
+       }
+       
+       
+       
+       int right[] = new int[N];
+       int max = A[N-1];
+       
+       for(int i = N-1; i>=0; i--){
+           if(A[i]>max){
+               max = A[i];
+           }
+           right[i] = max;
+       }
+       
+       
+       int i = 0;
+       int j = 0;
+       int ans = -1;
+       
+       while(i<N && j<N){
+           if(left[i]<=right[j]){
+               ans = Math.max(ans,j-i);
+               j++;
+           }
+           else{
+               i++;
+           }
+       }
+       return ans;
     }
 }
+
+
