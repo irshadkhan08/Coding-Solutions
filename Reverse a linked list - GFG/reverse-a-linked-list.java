3012290b6a8fe7fe4b59363fe27e7ec2c3797f9c
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-10-10 17:24:32 +0530

    Added solution - LeetHub

--- Reverse a linked list - GFG/reverse-a-linked-list.java
+++ Reverse a linked list - GFG/reverse-a-linked-list.java
@@ -71,15 +71,16 @@ class Solution
     Node reverseList(Node head)
     {
         
-    Node cur = head;
-    Node prev = null;
-    
-    while(cur!=null){
-       Node temp = new Node(cur.data);
-       temp.next = prev;
-       prev = temp;
-       cur = cur.next;
-    }
-    return prev;
+        Node cur = head;
+        Node prev = null;
+        
+        while(cur!=null){
+            Node temp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = temp;
+        }
+        return prev;
+ 
     }
 }
