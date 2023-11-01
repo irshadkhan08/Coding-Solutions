d958f8dedf9dae09c40fe02b35df3913f90aa4cd
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-11-02 01:55:32 +0530

    Added solution - LeetHub

--- Maximum of all subarrays of size k - GFG/maximum-of-all-subarrays-of-size-k.java
+++ Maximum of all subarrays of size k - GFG/maximum-of-all-subarrays-of-size-k.java
@@ -56,32 +56,27 @@ class Solution
     static ArrayList <Integer> max_of_subarrays(int arr[], int n, int k)
     {
         // Your code here
+        
         ArrayList<Integer>ans = new ArrayList<>();
-        
-       int i = 0;
-       int j = 0;
-       int max = 0;
-       
-       while(j<n){
-         max = Math.max(max,arr[j]);
-         
-         if(j-i+1<k){
-             j++;
-         }
-         else if(j-i+1==k){
-             ans.add(max);
-             if(arr[i]!=max){
-             i++;
-             j++;
-             }
-            else if(arr[i]==max){
-             i++;
-             j=i;
-             max = 0;
+        int i = 0;
+        int j = 0;
+        int max = Integer.MIN_VALUE;
+        while(j<n){
+            
+            if(j-i+1<k){
+                max = Math.max(max,arr[j]);
+                j++;
             }
-           }
-       }
-        
-       return ans;
+            else if(j-i+1>=k){
+                if(max<arr[j]){
+                    max = arr[j];
+                }
+                ans.add(max);
+                j = i+1;
+                i++;
+                max = 0;
+            }
+        }
+        return ans;
     }
 }
