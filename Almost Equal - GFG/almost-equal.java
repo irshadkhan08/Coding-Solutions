195632713b5a969e3dd70e8f43bf687f48f5357c
irshadkhan08 <81159691+irshadkhan08@users.noreply.github.com>
2023-12-07 17:09:13 +0530

    Added solution - LeetHub

---
+++ Almost Equal - GFG/almost-equal.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String s1 = br.readLine().trim();
+            String s2 = br.readLine().trim();
+            Solution obj = new Solution();
+            System.out.println(obj.countChars(s1, s2));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int countChars(String s1, String s2)
+    {
+        // Code here
+        c c1[] = new char[26];
+        char c2[] = new char[26];
+       
+       for(int i = 0; i<s1.length(); i++){
+           c1[s1.charAt(i)-'a']++;
+       }
+       
+       for(int i = 0; i<s2.length(); i++){
+           c2[s2.charAt(i)-'a']++;
+       }
+       
+       int ans = 0;
+       for(int i = 0; i<26; i++){
+           int v = Math.abs(c1[i]-c2[i]);
+            ans = ans+v;
+       }
+       return ans;
+    }
+}
